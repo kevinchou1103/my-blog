8aa54f98caf6087241ead74dd79bff382d98ddc1
DESKTOP-LQCF05T\kevin <zhoukw@yisa.com>
2021-03-19 17:56:47 +0800

    增加首页文章样式

---
+++ src/pages/detail/index.jsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+
+export default memo(function Detail() {
+  return (
+    <div className="Detail">
+      <div className="content">
+
+      </div>
+      <div className="author">
+
+      </div>
+    </div>
+  )
+})
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import Home from './home'
 import Mine from './mine'
 import Login from './login'
+import Detail from './detail'
 import NoPage from './noPage'
 
 export default {
   Home,
   Mine,
   Login,
-  NoPage
+  NoPage,
+  Detail
 }
